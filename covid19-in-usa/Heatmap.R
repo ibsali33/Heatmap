026b3def861bf9007918e420ca5d9bf34194ab43
Ibraheem Ali <43360964+ibsali33@users.noreply.github.com>
2020-04-02 11:28:32 -0700

    Add files via upload

--- covid19-in-usa/Heatmap.R
+++ covid19-in-usa/Heatmap.R
@@ -2,7 +2,9 @@
 
 library(dplyr)
 library(ggplot2)
+library(gplots)
 library(tidyr)
+library(tibble)
 
 #COVID19 data is a bit messy so we are going to need to do some data manipulation. Load two csv files the first containing the COVID19 data downloaded from "kaggle.com/sudalairajkumar/covid19-in-usa"
 
@@ -29,9 +31,6 @@ casespermillion <- tablemerge %>%
 forheatmap1 <- casespermillion %>%
   select(alldata.date, alldata.state, CPM)
 
-forheatmap3 <- forheatmap1 %>%
-  filter(alldata.state != "NY", alldata.state != "NJ")
-
 forheatmap2 <- casespermillion %>%
   select(alldata.date, alldata.state, alldata.positive)
 
@@ -46,16 +45,5 @@ forheatmapcpm <- forheatmap1 %>%
 forheatmapcpm[is.na(forheatmapcpm)] <- 0
 forheatmapcpm <- column_to_rownames(forheatmapcpm, var = "alldata.state")
 cpmmatrix <- as.matrix(forheatmapcpm)
-plot <- heatmap(cpmmatrix, scale="none", Colv = NA)
-
-
-
-forheatmapcpmnony <- forheatmap3 %>%
-  group_by(alldata.state, alldata.date) %>%
-  spread(key = alldata.date, value = CPM)
-
-forheatmapcpmnony[is.na(forheatmapcpmnony)] <- 0
-forheatmapcpmnony <- column_to_rownames(forheatmapcpmnony, var = "alldata.state")
-cpmnonymatrix <- as.matrix(forheatmapcpmnony)
-plot <- heatmap(cpmnonymatrix, scale="none", Colv = NA)
+cpmplot <- heatmap.2(cpmmatrix, scale="none", Colv = NA, breaks = c(0.5, 1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192), trace = "none", density.info = "none")
 
